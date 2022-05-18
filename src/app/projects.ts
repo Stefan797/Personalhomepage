8a3446c0d9d0eecb6756e4d7b28e04aef5e0fddb
Stefan797 <stefanhubner97@web.de>
2022-05-18 12:43:14 +0200

    change Project Images

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -3,7 +3,7 @@ export const allProjects = [
     name: 'Join Backend',
     descriptionphone: 'Aktuell Quellcode nur bei Github einsehbar.',
     description: 'A backend with login tasks objects rest-api and test Html at board. This is a project for a group project in the Frontend course.',
-    img: 'Join_backend_project_image.PNG',
+    img: 'joinbackend.png',
     url: 'https://kanbanboard12.herokuapp.com/api/task/1',
     doc: 'https://kanbanboard12.herokuapp.com/static/docs/_build/html/index.html'
   },
@@ -12,14 +12,14 @@ export const allProjects = [
     descriptionphone: 'Aktuell Quellcode nur bei Github einsehbar.',
     description: 'This simple chat messenger shows how quickly you can create a usable program of this kind with django. Just search for stefan_huebner in the settings and we can chat with each other there too.',
     // description: 'This simple chat messenger shows how quickly you can create a usable program of this kind with django. Just search for stefan_huebner in the settings and we can chat with each other there too.',
-    img: 'Django_chat_app_project_image.PNG',
+    img: 'djangochatapp.png',
     url: 'https://stefanh.pythonanywhere.com/',
     doc: 'https://stefanh.pythonanywhere.com/static/docs/_build/html/index.html'
   },
   {
     name: 'Flutter-sorts',
     description: 'This is a Kanbanboard built with angular. Notes can be sorted by projects, which helps you keep track of the boards. Entering the notes is also possible with the voice function. ',
-    img: 'Flutter_sorts_project_image.PNG',
+    img: 'kanban.jpg',
     url: 'https://stefanhuebner97.de/Fluttersorts/',
     doc: 'https://stefanhuebner97.de/Fluttersorts/documentation'
   },
@@ -27,21 +27,21 @@ export const allProjects = [
     name: 'El Pollo Loco',
     descriptionphone: 'This is a jump and run game',
     description: 'This is a Jump an Run Game. You have to jump over chickens and collect Tabasco. In the final level, you have to defeat the Pollo Loco.',
-    img: 'El_Polo_Loco_project_image.PNG',
+    img: 'elpololoco.png',
     url: 'https://stefanhuebner97.de/ElPolloLoco',
     doc: 'https://stefanhuebner97.de/ElPolloLoco/out'
   },
   {
     name: 'Pokedex',
     description: 'This Pokédex shows information about three generation of Pokémon.',
-    img: 'Pokédex_project_image.PNG',
+    img: 'pokédex.PNG',
     url: 'https://stefanhuebner97.de/Pokédex/',
     doc: 'https://stefanhuebner97.de/Pokédex/js/out'
   },
   {
     name: 'Kochwelt',
     description: 'Take a look at the recipes for the starters, main courses, and desserts. There are lots of information and preparation steps described.',
-    img: 'Kochwelt_project_image.PNG',
+    img: 'kochwelt.PNG',
     url: 'https://stefanhuebner97.de/Kochwelt',
     doc: 'https://stefanhuebner97.de/Kochwelt/out'
   }
